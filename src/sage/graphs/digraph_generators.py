126d370b774cffa11971ab414bfa40c521555852
Gregory McWhirter <gmcwhirt@uci.edu>
2010-04-20 23:50:28 -0700

    Trac 7608: Upgrade to NetworkX 1.0.1

--- src/sage/graphs/digraph_generators.py
+++ src/sage/graphs/digraph_generators.py
@@ -373,7 +373,7 @@ class DiGraphGenerators():
         if seed is None:
             seed = current_randstate().long_seed()
         import networkx
-        return DiGraph(networkx.gn_graph(n, kernel, seed))
+        return DiGraph(networkx.gn_graph(n, kernel, seed=seed))
 
     def RandomDirectedGNC(self, n, seed=None):
         """
@@ -409,7 +409,7 @@ class DiGraphGenerators():
         if seed is None:
             seed = current_randstate().long_seed()
         import networkx
-        return DiGraph(networkx.gnc_graph(n, seed))
+        return DiGraph(networkx.gnc_graph(n, seed=seed))
 
     def RandomDirectedGNP(self, n, p):
         r"""
@@ -479,7 +479,7 @@ class DiGraphGenerators():
         if seed is None:
             seed = current_randstate().long_seed()
         import networkx
-        return DiGraph(networkx.gnc_graph(n, seed))
+        return DiGraph(networkx.gnc_graph(n, seed=seed))
 
 ################################################################################
 #   DiGraph Iterators
